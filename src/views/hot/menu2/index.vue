408cfc9c5c605d3325f7671e2505dc94ff0945f4
fzu221801127 <1647688598@qq.com>
2022-03-27 19:16:17 +0800

    热门搜索页面

--- src/views/hot/menu2/index.vue
+++ src/views/hot/menu2/index.vue
@@ -1,59 +1,33 @@
 //在Echarts.vue文件中
 <template>
-  <div class="Echarts">
-    <div id="main" style="width: 600px;height: 400px;" />
+  <div :style="{backgroundImage: 'url(' + img + ')'}">
+    <div style="text-align:center;color:white;font-weight:bold">
+      本月热门搜索
+      <v-ThisMonthHotShearch />
+    </div>
+    <div style="margin-top:2%;text-align:center">
+      <v-HotShearch />
+    </div>
   </div>
 </template>
 
 <script>
+import ThisMonthHotShearch from '@/components/Table/ThisMonthHotShearch'
+import HotShearch from '@/components/Table/HotShearch'
 export default {
-  name: 'Echarts',
+  name: 'HotSearch',
+  components: {
+    'v-ThisMonthHotShearch': ThisMonthHotShearch,
+    'v-HotShearch': HotShearch
+  },
+  data() {
+    return {
+      img: require('@/assets/preview.jpg')
+    }
+  },
   mounted() {
-    this.myEcharts()
   },
   methods: {
-    myEcharts() {
-      var myChart = this.$echarts.init(document.getElementById('main'))
-      // 配置图表s
-      var option = {
-        title: {
-          text: 'echarts入门示例2'
-        },
-        dataset: [
-          {
-            dimensions: ['name', 'age', 'profession', 'score', 'date'],
-            source: [
-              ['Hannah Krause', 41, 'Engineer', 314, '2011-02-12'],
-              ['Zhao Qian', 20, 'Teacher', 351, '2011-03-01'],
-              ['Jasmin Krause ', 52, 'Musician', 287, '2011-02-14'],
-              ['Li Lei', 37, 'Teacher', 219, '2011-02-18'],
-              ['Karle Neumann', 25, 'Engineer', 253, '2011-04-02'],
-              ['Adrian Groß', 19, 'Teacher', '-', '2011-01-16'],
-              ['Mia Neumann', 71, 'Engineer', 165, '2011-03-19'],
-              ['Böhm Fuchs', 36, 'Musician', 318, '2011-02-24'],
-              ['Han Meimei', 67, 'Engineer', 366, '2011-03-12']
-            ]
-          },
-          {
-            transform: {
-              type: 'sort',
-              config: { dimension: 'score', order: 'desc' }
-            }
-          }
-        ],
-        xAxis: {
-          type: 'category',
-          axisLabel: { interval: 0, rotate: 30 }
-        },
-        yAxis: {},
-        series: {
-          type: 'bar',
-          encode: { x: 'name', y: 'score' },
-          datasetIndex: 1
-        }
-      }
-      myChart.setOption(option)
-    }
   }
 }
 </script>
